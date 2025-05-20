d129a1fc32f3d3a9112a0f388b413a9d029583a9
915529889@qq.com <houdonghui@yhbj.com>
2025-05-20 13:43:55 +0800

    初始化

---
+++ Example/DHWebBrige/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DHWebBrige
+//
+//  Created by 915529889@qq.com on 05/19/2025.
+//  Copyright (c) 2025 915529889@qq.com. All rights reserved.
+//
+
+import UIKit
+import DHWebBrigeKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/DHWebBrige/DHWebBrige-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DHWebBrige : NSObject
+@end
+@implementation PodsDummy_DHWebBrige
+@end
